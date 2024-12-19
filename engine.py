64f0de50853dbc1b66a42d3e39aa61b3ebfcbac3
frederik.schaaf <frederik.schaaf@quantrefy.com>
2024-12-19 15:43:17 +0100

    finished writing code, bugs to be fixed

--- engine.py
+++ engine.py
@@ -1,54 +1,116 @@
+import os
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
-    def __init__(self):
+    def __init__(self, include_dir):
         super().__init__()
         self.convert_charrefs = False
         self.result = []
+        self.include_dir = include_dir
+        self.engine_var_parsers = {}
+        self.engine_current_parser = None
 
     def handle_starttag(self, tag, attrs):
-        attrs_str = " ".join(f'{name}="{value}"' for name, value in attrs)
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_starttag(tag, attrs)
+            return
+        
+        if tag == "engine":
+            self.handle_engine_start(attrs)
+            return
+
+        attrs_str = " ".join(f'{key}="{value}"' for key, value in attrs)
         if attrs_str:
             self.result.append(f"<{tag} {attrs_str}>")
         else:
             self.result.append(f"<{tag}>")
 
     def handle_endtag(self, tag):
+        if tag == "engine":
+            self.handle_engine_end()
+            return
         self.result.append(f"</{tag}>")
 
     def handle_startendtag(self, tag, attrs):
-        attrs_str = " ".join(f'{name}="{value}"' for name, value in attrs)
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_startendtag(tag, attrs)
+            return
+
+        if tag == "engine":
+            self.handle_engine_start(attrs)
+            self.handle_engine_end()
+            return
+        
+        attrs_str = " ".join(f'{key}="{value}"' for key, value in attrs)
         if attrs_str:
             self.result.append(f"<{tag} {attrs_str} />")
         else:
             self.result.append(f"<{tag} />")
 
     def handle_data(self, data):
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_data(data)
+            return
+        
         self.result.append(data)
 
     def handle_entityref(self, name):
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_entityref(name)
+            return
+        
         self.result.append(f"&{name};")
 
     def handle_charref(self, name):
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_charref(name)
+            return
+        
         self.result.append(f"&#{name};")
+
+    def handle_engine_start(self, attrs):
+        if self.engine_current_parser is not None:
+            self.engine_current_parser.handle_engine_start(attrs)
+            return
+        
+        for key, value in attrs:
+            if key == "var":
+                self.engine_current_parser = MyHTMLParser(self.include_dir)
+                self.engine_var_parsers[value] = self.engine_current_parser
+            elif key == "insert":
+                if value == "file":
+                    filename = attrs["name"]
+                    filepath = os.path.join(self.include_dir, filename)
+                    parser = MyHTMLParser(self.include_dir)
+                    with open(filepath, 'r', encoding='utf-8') as file:
+                        html_content = file.read()
+                    parser.engine_var_parsers = self.engine_var_parsers
+                    parser.feed(html_content)
+                    self.result.append(parser.emit_ħtml())
+                elif value == "var":
+                    name = attrs["name"]
+                    self.result.append(self.engine_var_parsers[name].emit_html())
+
+    def handle_engine_end(self):
+        if self.engine_current_parser is None:
+            return False
+        if self.engine_current_parser.handle_engine_end():
+            return True
+        self.engine_current_parser = None
+        return True
 
     def emit_html(self):
         return "".join(self.result)
 
-
-# Read an HTML file
-file_path = 'signature.html'  # Replace with your file path
-with open(file_path, 'r', encoding='utf-8') as file:
-    html_content = file.read()
-
-# Create an instance of the parser and feed the content
-parser = MyHTMLParser()
-parser.feed(html_content)
-
-emit_file_path = 'signature_emit.html'  # Replace with your file path
-with open(emit_file_path, 'w', encoding='utf-8') as file:
-    html_content = parser.emit_html()
-    print("this is sick: " + html_content + ". yes.")
-    file.write(html_content)
-
+def parse_file(filename, dir, include_dir, output_dir):
+    file_path = os.path.join(dir, filename)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    parser = MyHTMLParser(include_dir)
+    parser.feed(html_content)
+    output_content = parser.emit_html()
+    outfile_path = os.path.join(output_dir, filename)
+    with open(outfile_path, 'w', encoding='utf-8') as file:
+        file.write(output_content)
+    
 
---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import engine
+
+sigs_folder = "sigs"
+include_folder = os.path.join(sigs_folder, "include")
+output_folder = "output"
+
+os.makedirs(output_folder, exist_ok=True)
+
+files = [f for f in os.listdir(sigs_folder) if os.path.isfile(os.path.join(sigs_folder, f))]
+
+for file in files:
+    filepath = os.path.join(sigs_folder, file)   
+    engine.parse_file(file, sigs_folder, include_folder, output_folder)
+
